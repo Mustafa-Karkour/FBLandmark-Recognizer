60efa835b1a6353990bb170ab64783436828062c
notsom1 <S00052749@auk.edu.kw>
2021-04-24 18:02:30 +0300

    Added NearbyPlaces

--- app/src/main/java/com/example/landmarkfb/NearbyPlaces.kt
+++ app/src/main/java/com/example/landmarkfb/NearbyPlaces.kt
@@ -7,6 +7,7 @@ import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
